df342cde9fd23a35e537803e59e9b3abc98cba26
Danil Borchevkin <danil.borchevkin@gmail.com>
2019-03-18 09:02:44 +0200

    WIP

--- wavelet_heatmap.py
+++ wavelet_heatmap.py
@@ -1,6 +1,7 @@
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
+from defaultlist import defaultlist
 
 def f(x):
     return (x[:,0]**2 + x[:,1]**2)
@@ -11,11 +12,29 @@ def get_graph_from_file(in_folder, in_filename, out_folder, out_filename):
     
     # Prepare data
     # TODO there is some issue
+    # In one row - 586 values
+    # Overall there are 30 rows
+    # So we need create matrix from it data like this
+    #Z = [
+    #    [0,1,2],
+    #    [1,2,3],
+    #    [4,5,6]
+    #]
+
+    Z = defaultlist(lambda: [])
+    current_period = period[0]
+    current_row = 0
+
+    for i in range(len(t)):
+        if current_period != period[i]:
+            current_row += 1
+            current_period = period[i]
+        Z[current_row].append(wavelet[i])
 
     # Create graph
     plt.figure()
-    cp = plt.contourf(X, Y, Z)
-    #cp = plt.pcolor(X, Y, Z)
+    cp = plt.contourf(Z, 1000, cmap='RdGy')
+    #cp = plt.pcolor(Z)
     plt.colorbar(cp)
     plt.title('Wavelet Data')
     plt.xlabel('Period, min')
